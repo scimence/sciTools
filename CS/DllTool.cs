39008a685a0e61a76df0eae1ff58c67810f9352a
scimence <scimence@163.com>
2018-08-22 19:44:17 +0800

    2018-08-22_19.44.17

--- CS/DllTool.cs
+++ CS/DllTool.cs
@@ -7,21 +7,51 @@ using System.Threading.Tasks;
 
 namespace SciTools
 {
+
+    //static class Program
+    //{
+    //    /// <summary>
+    //    /// 应用程序的主入口点。
+    //    /// </summary>
+    //    [STAThread]
+    //    static void Main()
+    //    {
+    //        DllTool.LoadResourceDll();                // 载入依赖dll
+    //        _Main();
+    //    }
+
+    //    static void _Main()
+    //    {
+    //        SciUpdate.RunningExceptionTool.Run(call);   // 调用异常信息捕获类，进行异常信息的捕获
+    //    }
+
+    //    // 应用程序，入口逻辑
+    //    public static void call(string[] args)
+    //    {
+    //        //UpdateTool.AutoUpdate();
+    //        Application.EnableVisualStyles();
+    //        Application.SetCompatibleTextRenderingDefault(false);
+
+    //        Application.Run(new Form1());
+    //    }
+    //}
+
     /// <summary>
     /// 载入Resources中附带的dll文件，
     /// SciTools.DllTool.LoadResourceDll();
     /// C# 在EXE应用中集成dll https://blog.csdn.net/scimence/article/details/79018812
     /// </summary>
-    class DllTool
+    public class DllTool
     {
         /// <summary>
         /// 载入资源文件中附带的所有dll文件
+        /// 此函数需和dll函数调用前执行，且不在同一个函数体内
         /// </summary>
         public static void LoadResourceDll()
         {
             AppDomain.CurrentDomain.AssemblyResolve += new ResolveEventHandler(CurrentDomain_AssemblyResolve);
         }
- 
+
         private static System.Reflection.Assembly CurrentDomain_AssemblyResolve(object sender, ResolveEventArgs args)
         {
             string dllName = args.Name.Contains(",") ? args.Name.Substring(0, args.Name.IndexOf(',')) : args.Name.Replace(".dll", "");
@@ -32,6 +62,5 @@ namespace SciTools
             byte[] bytes = (byte[])rm.GetObject(dllName);
             return System.Reflection.Assembly.Load(bytes);
         }
- 
     }
 }
